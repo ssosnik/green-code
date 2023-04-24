f68f971c789a65d8959b323ca6c828bb0989d717
ssosnik <s.sosnik@gmail.com>
2023-04-24 16:28:51 +0200

    add build and run shell scripts update README

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# clean and package the project with maven
+mvn clean package
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# run the Spring Boot application
+java -jar target/ing-green-services-api-1.0.0-SNAPSHOT.jar
